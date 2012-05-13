65551bd26c5de856f745588111d29b349e4c3875
István Huszár <istvan@e-channel.com.au>
2012-05-13 15:11:29 +0930

    Add Compass require

--- app.rb
+++ app.rb
@@ -1,16 +1,18 @@
 require 'rubygems'
 require 'sinatra'
 require 'haml'
+require 'compass'
 
 # Helpers
 require './lib/render_partial'
 
 # Set Sinatra variables
 set :app_file, __FILE__
-set :root, File.dirname(__FILE__)
-set :views, 'views'
-set :public, 'public'
-set :haml, {:format => :html5} # default Haml format is :xhtml
+set :root    , File.dirname(__FILE__)
+set :views   , 'views'
+set :public  , 'public'
+set :haml    , { format: :html5}
+set :sass    , Compass.sass_engine_options
 
 # Application routes
 get '/' do
